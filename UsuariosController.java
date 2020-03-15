b21605027246413741d9a834f9dbe0b5e993515b
jimmy-ugb <58155232+jimmy-ugb@users.noreply.github.com>
2020-03-15 12:33:18 -0600

    se agregan cambios a en usuarioscontrller y ubicacionescontroller
    se agregan cambios a en usuarioscontrller y ubicacionescontroller

---
+++ UsuariosController.java
@@ -0,0 +1,48 @@
+﻿package com.powersoft.sigec.models.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.powersoft.sigec.model.entities.Usuarios;
+import com.powersoft.sigec.models.repository.UsuariosRepository;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuariosController {
+	@Autowired
+	public UsuariosRepository repository;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public List<Usuarios> getUsuarios(){
+		 List<Usuarios> listado = (List<Usuarios>) repository.findAll();
+		 return listado;
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public Optional<Usuarios> obtenerCliente(@PathVariable("id") Long id){
+	Optional<Usuarios> UsuariosObjeto = repository.findById(id);
+	return UsuariosObjeto;
+	}
+	@RequestMapping(value="/", method = RequestMethod.POST)
+	public Usuarios add(@RequestBody Usuarios entity){
+	Usuarios objetoUsuarios = repository.save(entity);
+	return objetoUsuarios;
+	}
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	public void deleteById(@PathVariable("id") Long id){
+	repository.deleteById(id);
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+    public Usuarios login(@RequestBody(required = true)Usuarios entity){
+        return repository.login(entity.getCorreo(), entity.getContraseña());
+    }
+
+}
